5978e233ef43508448dc80ff438a2d3dd1189940
Lucas Chaves <lucasvchaves@gmail.com>
2023-12-05 21:28:15 -0300

    Limpa a caixa de texto apos inserir o conteudo

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 
+btnAdicionar.addEventListener('click', adicionaItemDaEntrada);
+
+function adicionaItemDaEntrada() {
+    var conteudo = pEntrada.innerText;
+
+    const li = document.createElement('li');
+    li.textContent = conteudo;
+
+    olItens.appendChild(li);
+    pEntrada.innerText = "";
+}
+
 btnLimpar.addEventListener('click', limparItensDeLista);
 
 function limparItensDeLista() {
